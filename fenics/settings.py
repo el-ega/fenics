6c11207599a97d5181b75062c1fc21437ff4cb22
matiasb <mbordese@gmail.com>
2014-03-22 10:51:02 -0300

    Added base template, setup bootstrap static files.

--- fenics/settings.py
+++ fenics/settings.py
@@ -82,3 +82,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
+
