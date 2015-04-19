fafa2bcadb12ffe655e8c5fd1d030bb8a928a16d
nessita <nataliabidart@gmail.com>
2015-04-19 16:28:04 -0300

    Added missing empty spaces.

--- ega/tests/test_models.py
+++ ega/tests/test_models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.core import mail
 from django.test import TestCase
@@ -6,6 +8,7 @@ from ega.models import EgaUser
 from ega.constants import INVITE_SUBJECT, INVITE_BODY
 
 ADMINS = ['natalia@gmail.com', 'matias@gmail.com']
+
 
 class EgaUserTestCase(TestCase):
 
