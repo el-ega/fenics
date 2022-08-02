413394e1963b7781f3ff97231eeeede9bebb177a
Natalia <nataliabidart@gmail.com>
2022-08-02 15:51:58 -0300

    Extend default prediction so penalties can also be stored

--- ega/tests/test_models.py
+++ ega/tests/test_models.py
@@ -1,7 +1,8 @@
-from __future__ import unicode_literals
+from unittest import mock
 
 from django.conf import settings
 from django.core import mail
+from django.utils.timezone import now
 
 from ega.constants import INVITE_SUBJECT, INVITE_BODY
 from ega.tests.helpers import TestCase
@@ -51,10 +52,19 @@ class EgaUserTestCase(TestCase):
         user = self.factory.make_user()
 
         self.assertIsNone(user.default_prediction, None)
-        user.default_prediction = (1, 0)
+
+        right_now = now()
+        with mock.patch('ega.models.now', return_value=right_now):
+            user.default_prediction = (1, 0, '')
 
         self.assertEqual(
-            user.default_prediction, {'home_goals': 1, 'away_goals': 0}
+            user.default_prediction,
+            {
+                'home_goals': 1,
+                'away_goals': 0,
+                'penalties': '',
+                'last_updated': right_now,
+            },
         )
         self.assertEqual(
             user.preferences, {'default_prediction': user.default_prediction}
@@ -62,18 +72,28 @@ class EgaUserTestCase(TestCase):
 
 
 class UpdateRelatedPredictions(TestCase):
-    def finish_match(self, match, home_goals=0, away_goals=0):
+    def finish_match(
+        self,
+        match,
+        home_goals=0,
+        away_goals=0,
+        pk_home_goals=None,
+        pk_away_goals=None,
+    ):
         match.home_goals = home_goals
         match.away_goals = away_goals
+        match.pk_home_goals = pk_home_goals
+        match.pk_away_goals = pk_away_goals
         match.finished = True
         match.save()
 
     def assert_prediction_correct(
-        self, prediction, away_goals, home_goals, score, source
+        self, prediction, away_goals, home_goals, penalties, score, source
     ):
         prediction.refresh_from_db()
         self.assertEqual(prediction.away_goals, away_goals)
         self.assertEqual(prediction.home_goals, home_goals)
+        self.assertEqual(prediction.penalties, penalties)
         self.assertEqual(prediction.score, score)
         self.assertEqual(prediction.source, source)
 
@@ -88,11 +108,16 @@ class UpdateRelatedPredictions(TestCase):
         for p in predictions:
             with self.subTest(prediction=p):
                 self.assert_prediction_correct(
-                    p, away_goals=None, home_goals=None, score=0, source='web'
+                    p,
+                    away_goals=None,
+                    home_goals=None,
+                    penalties='',
+                    score=0,
+                    source='web',
                 )
 
     def test_update_unpredicted_prediction(self):
-        default = {'home_goals': 1, 'away_goals': 0}
+        default = {'home_goals': 1, 'away_goals': 0, 'penalties': ''}
         user = self.factory.make_user(
             preferences={'default_prediction': default}
         )
@@ -108,6 +133,30 @@ class UpdateRelatedPredictions(TestCase):
             prediction,
             home_goals=1,
             away_goals=0,
+            penalties='',
+            score=3,
+            source='preferences',
+        )
+
+    def test_update_unpredicted_prediction_with_penalties(self):
+        default = {'home_goals': 1, 'away_goals': 1, 'penalties': 'L'}
+        user = self.factory.make_user(
+            preferences={'default_prediction': default}
+        )
+        assert user.default_prediction == default, user.default_prediction
+        match = self.factory.make_match()
+        prediction = self.factory.make_prediction(match=match, user=user)
+        assert prediction.home_goals is None
+        assert prediction.away_goals is None
+        assert prediction.penalties == ''
+
+        self.finish_match(match, 1, 1)
+
+        self.assert_prediction_correct(
+            prediction,
+            home_goals=1,
+            away_goals=1,
+            penalties='L',
             score=3,
             source='preferences',
         )
@@ -127,7 +176,12 @@ class UpdateRelatedPredictions(TestCase):
         self.finish_match(match, 1, 0)
 
         self.assert_prediction_correct(
-            prediction, home_goals=2, away_goals=1, score=1, source='web'
+            prediction,
+            home_goals=2,
+            away_goals=1,
+            penalties='',
+            score=1,
+            source='web',
         )
 
     def test_update_unpredicted_predictions_many(self):
@@ -184,6 +238,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_no_pref_unpredicted,
             home_goals=None,
             away_goals=None,
+            penalties='',
             score=0,
             source='web',
         )
@@ -191,6 +246,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_predicted_exact_result,
             home_goals=1,
             away_goals=2,
+            penalties='',
             score=3,
             source='web',
         )
@@ -198,6 +254,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_predicted_exact_side,
             home_goals=0,
             away_goals=2,
+            penalties='',
             score=1,
             source='web',
         )
@@ -205,6 +262,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_predicted_mismatch,
             home_goals=1,
             away_goals=0,
+            penalties='',
             score=0,
             source='web',
         )
@@ -212,6 +270,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_no_default_prediction,
             home_goals=None,
             away_goals=None,
+            penalties='',
             score=0,
             source='web',
         )
@@ -220,6 +279,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_default_exact_result,
             home_goals=1,
             away_goals=2,
+            penalties='',
             score=3,
             source='preferences',
         )
@@ -227,6 +287,7 @@ class UpdateRelatedPredictions(TestCase):
             prediction_default_exact_side,
             home_goals=2,
             away_goals=5,
+            penalties='',
             score=1,
             source='preferences',
         )
@@ -234,6 +295,144 @@ class UpdateRelatedPredictions(TestCase):
             prediction_default_mismatch,
             home_goals=1,
             away_goals=0,
+            penalties='',
             score=0,
+            source='preferences',
+        )
+
+    def test_update_unpredicted_prediction_many_with_penalties(self):
+        user_buggy_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 1,
+                    'away_goals': 1,
+                    'penalties': 'a',  # intentionally buggy
+                }
+            }
+        )
+        user_exact_score_and_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 1,
+                    'away_goals': 1,
+                    'penalties': 'L',
+                }
+            }
+        )
+        user_tie_and_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 0,
+                    'away_goals': 0,
+                    'penalties': 'L',
+                }
+            }
+        )
+        user_exact_score_incorrect_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 1,
+                    'away_goals': 1,
+                    'penalties': 'V',
+                }
+            }
+        )
+        user_tie_incorrect_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 0,
+                    'away_goals': 0,
+                    'penalties': 'V',
+                }
+            }
+        )
+        user_exact_score_empty_penalties = self.factory.make_user(
+            preferences={
+                'default_prediction': {
+                    'home_goals': 1,
+                    'away_goals': 1,
+                    'penalties': '',
+                }
+            }
+        )
+
+        match = self.factory.make_match()
+
+        no_default_prediction = self.factory.make_prediction(match=match)
+        buggy_penalties = self.factory.make_prediction(
+            match=match, user=user_buggy_penalties
+        )
+        exact_score_and_penalties = self.factory.make_prediction(
+            match=match, user=user_exact_score_and_penalties
+        )
+        tie_and_penalties = self.factory.make_prediction(
+            match=match, user=user_tie_and_penalties
+        )
+        exact_score_incorrect_penalties = self.factory.make_prediction(
+            match=match, user=user_exact_score_incorrect_penalties
+        )
+        tie_incorrect_penalties = self.factory.make_prediction(
+            match=match, user=user_tie_incorrect_penalties
+        )
+        exact_score_empty_penalties = self.factory.make_prediction(
+            match=match, user=user_exact_score_empty_penalties
+        )
+
+        self.finish_match(match, 1, 1, 5, 4)  # tie with a 'home' penalty win
+
+        self.assert_prediction_correct(
+            no_default_prediction,
+            home_goals=None,
+            away_goals=None,
+            penalties='',
+            score=0,
+            source='web',
+        )
+        self.assert_prediction_correct(
+            buggy_penalties,
+            home_goals=1,
+            away_goals=1,
+            penalties='',
+            score=3,
+            source='preferences',
+        )
+        self.assert_prediction_correct(
+            exact_score_and_penalties,
+            home_goals=1,
+            away_goals=1,
+            penalties='L',
+            score=4,
+            source='preferences',
+        )
+        self.assert_prediction_correct(
+            tie_and_penalties,
+            home_goals=0,
+            away_goals=0,
+            penalties='L',
+            score=2,
+            source='preferences',
+        )
+        self.assert_prediction_correct(
+            exact_score_incorrect_penalties,
+            home_goals=1,
+            away_goals=1,
+            penalties='V',
+            score=3,
+            source='preferences',
+        )
+        self.assert_prediction_correct(
+            tie_incorrect_penalties,
+            home_goals=0,
+            away_goals=0,
+            penalties='V',
+            score=1,
+            source='preferences',
+        )
+        self.assert_prediction_correct(
+            exact_score_empty_penalties,
+            home_goals=1,
+            away_goals=1,
+            penalties='',
+            score=3,
             source='preferences',
         )
